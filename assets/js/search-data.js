0a3a22e551ba1f0ee27cc124f319ac1a41d34ed8
youngung <youngung.jeong@gmail.com>
2025-08-28 22:55:53 +0000

    Deploying to gh-pages from @ youngung/youngung.github.io@86d58d766b405e0d0bb018d2dbbe2931de417014 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,21 +58,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-nist-교환-학생-소식",
+        },{id: "post-nist-교환-학생-소식",
         
           title: "NIST 교환 학생 소식",
         
