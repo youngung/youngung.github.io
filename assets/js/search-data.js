576e2ee135d163c162edf836375a0050b346cc82
youngung <youngung.jeong@gmail.com>
2025-08-28 06:15:24 +0000

    Deploying to gh-pages from @ youngung/youngung.github.io@9b14a9caac84d7a6d62f75ee67343e38169b265b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -594,16 +594,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-홈페이지를-개편하고-있습니다",
+          title: '홈페이지를 개편하고 있습니다.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
